57e0000aec5fe7e8f93fe596c8351648f4fac8f5
Taxscape <sam@taxscape.ai>
2025-12-23 17:29:19 -0800

    Add redirect from apex domain to www.taxscape.ai

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     typedRoutes: true
-  }
+  },
+  async redirects() {
+    return [
+      // Redirect apex domain to www
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'taxscape.ai',
+          },
+        ],
+        destination: 'https://www.taxscape.ai/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
